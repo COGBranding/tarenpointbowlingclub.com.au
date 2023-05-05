202ab4aca76f725007036007c2d3a5fdf09e8c0f
Blake <contact@blakezajac.com>
2023-05-05 14:34:43 +1000

    Update popup.js to allow for multiple popups

--- js/popup.js
+++ js/popup.js
@@ -1,43 +1,54 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    const singlePopup = document.querySelector(".single-popup");
+    const singlePopup = document.querySelectorAll(".pum");
+    const popupOpen = document.querySelector("html");
 
     if (singlePopup) {
-        const popupOpen = document.querySelector("html");
-        const popupParent = document.querySelector(".pum");
-        const popupContainer = document.querySelector(".pum-container");
-        const popupClose = document.querySelector(".pum-close");
-        const popupHead = document.querySelector(
-            ".single-popup__head__flex-container"
-        );
+        singlePopup.forEach((popup) => {
+            popup.classList.add("single-popup__section");
+            const popupContainer = popup.querySelector(".pum-container");
 
-        popupParent.classList.add("single-popup__section");
-        popupContainer.classList.add("single-popup__container");
-        popupHead.appendChild(popupClose);
+            const popupClose = popup.querySelector(".pum-close");
+            const popupHead = popup.querySelector(
+                ".single-popup__head__flex-container"
+            );
 
-        const buttonWrappers = document.querySelectorAll(
-            ".single-popup .et_pb_button_module_wrapper"
-        );
-        const wrapperDiv = document.createElement("div");
-        wrapperDiv.classList.add("single-popup__button-wrapper");
+            popupContainer.classList.add("single-popup__container");
+            popupHead.appendChild(popupClose);
 
-        buttonWrappers.forEach((buttonWrapper) => {
-            wrapperDiv.appendChild(buttonWrapper);
-        });
+            const buttonWrappers = popup.querySelectorAll(
+                ".single-popup .et_pb_button_module_wrapper"
+            );
+            const wrapperDiv = document.createElement("div");
+            wrapperDiv.classList.add("single-popup__button-wrapper");
 
-        const contentRowCol = document.querySelector(
-            ".single-popup__content-row__col--content"
-        );
-        contentRowCol.appendChild(wrapperDiv);
+            if (buttonWrappers) {
+                buttonWrappers.forEach((buttonWrapper) => {
+                    wrapperDiv.appendChild(buttonWrapper);
+                });
+            }
 
-        const btnClose = document.querySelector(".single-popup .btn--white");
+            const contentRowCol = popup.querySelector(
+                ".single-popup__content-row__col--content"
+            );
+            contentRowCol.appendChild(wrapperDiv);
 
-        btnClose.addEventListener("click", (event) => {
-            popupParent.style.display = "none";
-            popupParent.style.opacity = "0";
+            const btnClose = popup.querySelector(".single-popup .btn--white");
 
-            // Remove classes from <html> as it prevented scrolling when the popup was closed
-            popupOpen.classList.remove("pum-open-overlay");
-            popupOpen.classList.remove("pum-open-scrollable");
+            if (btnClose) {
+                btnClose.addEventListener("click", (event) => {
+                    //change css to child
+                    popupContainer.style.display = "none";
+                    popupContainer.style.opacity = "0";
+
+                    //then change css to parent
+                    popup.style.display = "none";
+                    popup.style.opacity = "0";
+
+                    // Remove classes from <html> as it prevented scrolling when the popup was closed
+                    popupOpen.classList.remove("pum-open-overlay");
+                    popupOpen.classList.remove("pum-open-scrollable");
+                });
+            }
         });
     }
 });
